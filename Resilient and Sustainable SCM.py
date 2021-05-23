eb66a72df2a529f916fefdc7bbcbf5864f2f268c
maximilianjanetschek <gonrur-zunwih-9kEsca>
2021-05-23 20:55:47 +0200

    updated solution

--- Resilient and Sustainable SCM.py
+++ Resilient and Sustainable SCM.py
@@ -1,59 +1,58 @@
 import gurobipy as gurobi
+import numpy as np
 
 
 # Data import
 ''' Index '''
-# i Primary Supplier
-# j Backup Supplier
-# m manufacturers
-# a distributors
-# b markets
-# c collectors
-# r recyclers
-# p tire index
-# w raw material
-# s distribution scenario
-# f information shared
+i = 0   # i Primary Supplier
+j = 0   # j Backup Supplier
+m = 0   # m manufacturers
+a = 0   # a distributors
+b = 0   # b markets
+c = 0   # c collectors
+r = 0   # r recyclers
+p = 0   # p tire index
+w = 0   # w raw material
+s = 0   # s distribution scenario
+f = 0   # f information shared
 
 ''' Parameters '''
-RC_w_ij
-CC_ij
-LC_p_m
-p_s # disruption probability in scenario s
-N_i_s # disrupted capacity of manufactuer
-G_w # quantity for producing one final product
-T_w_ij_m^s # transportation cost of material w to supplier manufacturer m in scenario s
-T_p_m_a^s # manufacturer to distributor
-T_p_a_b^s # distributor to market
-T_p_b_c^s # market to collector
-T_p_c_r^s # collector to recycler
-T_r_m^s # recycler manufacturer
-Fixed_cost # for manufacturer, distributor, collector, recycler
-Capacity^s # for primary supplier, backup supplier, manufacturer, distributor, collecotr, recyler
-Purchasing Price
-Demand
-Percentage Scrapped
-Pollution_Emitted
-Water_Consumption # manufactuer, recycler
-Electricity_Consumption # manufactuer, recycler
-Pollution # manufacturer, recycler
-Fixed_Job_Opportunities # supplier, manufacturer, distributor, collector, recycler
-Variable_Job_Opportunities # supplier, manufacturer, distributor, collector, recycler
-Security_System^s #
-information_sharing_f^s # cost information sharing of kind f
-percentage_of_information_exchange
+RawMaterial_Cost_PrimarySupplier = np.ndarray(shape=(w,i), dtype=float)
+RawMaterial_Cost_BackupSupplier = np.ndarray(shape=(w,j),dtype=float)
+Contracting_Cost_PrimarySupplier = np.ndarray(shape=(i),dtype=float)
+Contracting_Cost_BackupSupplier = np.ndarray(shape=(j),dtype=float)
+Production_Cost_Manufacturer = np.ndarray(shape=(p,m), dtype=float)
+Disruption_probability = np.ndarray(shape=(s), dtype=float)                     # disruption probability in scenario s
+Disrupted_Capacity_PrimarySupplier = np.ndarray(shape=(i,s), dtype=float)       # disrupted capacity of manufactuer
+RawMaterial_for_Tire = np.ndarray(shape=(i,s), dtype=float)                     # quantity for producing one final product
+T_w_ij_m_s = np.ndarray(shape=(i,s), dtype=float)                               # transportation cost of material w to supplier manufacturer m in scenario s
+T_p_m_a_s = np.ndarray(shape=(i,s), dtype=float)                                # manufacturer to distributor
+T_p_a_b_s = np.ndarray(shape=(i,s), dtype=float)                                # distributor to market
+T_p_b_c_s = np.ndarray(shape=(i,s), dtype=float)                                # market to collector
+T_p_c_r_s = np.ndarray(shape=(i,s), dtype=float)                                # collector to recycler
+T_r_m_s = np.ndarray(shape=(i,s), dtype=float)                                  # recycler manufacturer
+Fixed_cost= np.ndarray(shape=(i,s), dtype=float)                                # for manufacturer, distributor, collector, recycler
+Capacity_s = np.ndarray(shape=(i,s), dtype=float)                               # for primary supplier, backup supplier, manufacturer, distributor, collecotr, recyler
+Purchasing_Price= np.ndarray(shape=(i,s), dtype=float)
+Demand= np.ndarray(shape=(i,s), dtype=float)
+Percentage_Scrapped= np.ndarray(shape=(i,s), dtype=float)
+Pollution_Emitted= np.ndarray(shape=(i,s), dtype=float)
+Water_Consumption= np.ndarray(shape=(i,s), dtype=float)                         # manufactuer, recycler
+Electricity_Consumption= np.ndarray(shape=(i,s), dtype=float)                   # manufactuer, recycler
+Pollution= np.ndarray(shape=(i,s), dtype=float)                                 # manufacturer, recycler
+Fixed_Job_Opportunities= np.ndarray(shape=(i,s), dtype=float)                   # supplier, manufacturer, distributor, collector, recycler
+Variable_Job_Opportunities= np.ndarray(shape=(i,s), dtype=float)                # supplier, manufacturer, distributor, collector, recycler
+Security_System_s = np.ndarray(shape=(i,s), dtype=float)                        #
+information_sharing_f_s= np.ndarray(shape=(i,s), dtype=float)                   # cost information sharing of kind f
+percentage_of_information_exchange= np.ndarray(shape=(i,s), dtype=float)
 
-
-
-
-
-
-
-
-
-# Model
+'''Optimization Model'''
 SCM_Model = gurobi.Model('Resilient and Sustainable SCM')
 
-# Optimization
+'''Decision Variables'''
 
-# Solution
+'''Objective Functions'''
+
+'''Constraints'''
+
+'''Optimization'''
