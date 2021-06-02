e86ec5dfba69dc0c9fc72a2ccd7c28d402ec4703
Maximilian Janetschek <61047435+MaximilianJanetschek@users.noreply.github.com>
2021-06-02 22:55:43 +0200

    added data retrieval

---
+++ Data/DataManipulation.py
@@ -0,0 +1,45 @@
+# get market distances
+
+# PLZ data from https://www.suche-postleitzahl.org/downloads
+# summarize into landkreise as only 126 remain -> should be reasonable to model market
+import csv
+
+def get_population_by_district() -> dict():
+    plz_dict = {}
+
+    # retrieve all plz and area names
+    with open('zuordnung_plz_ort_landkreis.csv', newline='') as csvfile:
+        zip_code_city = csv.DictReader(csvfile, delimiter=',')
+        for row in zip_code_city:
+            plz_dict[row['plz']] = {'landkreis':row['landkreis'], 'population':0}
+
+    # get citizens per zip code
+    with open ('plz_einwohner.csv', newline='') as csvfile:
+        plz_einwohner = csv.DictReader(csvfile, delimiter= ',')
+        for row in plz_einwohner:
+            if row['plz'] in plz_dict.keys():
+                plz_dict[row['plz']]['population'] = plz_dict[row['plz']]['population'] + int(row['einwohner'])
+            else:
+                print('Left out:')
+                print(row['plz'] + ' with a population of '+  row['einwohner'])
+
+    # sort population to area
+    district_population = {}
+    for zipcode in plz_dict.values():
+        if zipcode['landkreis'] in district_population.keys():
+            district_population[zipcode['landkreis']] = district_population[zipcode['landkreis']] + zipcode['population']
+        else:
+            district_population[zipcode['landkreis']] = zipcode['population']
+
+    quick_test = 0
+    for i in district_population.values():
+        quick_test = quick_test + i
+
+    print(quick_test)
+
+    return district_population
+
+
+
+
+
---
+++ Utility/Distance_and_Map.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import googlemaps
+
+
+locations = pd.DataFrame({"origin_city": ["Hamburg", "Toronto", "Sucre",
+                                          "Jaipur", "Wellington"],
+                          "origin_country": ["Germany", "Canada", "Bolivia",
+                                      "India", "New Zealand"],
+                          "destination_city": ["Zurich", "New York", "Santiago",
+                                              "Bengaluru", "Auckland"],
+                          "destination_country": ["Switzerland", "USA", "Chile",
+                                                 "India", "New Zealand"]
+                         },
+                        columns = ["origin_city", "origin_country",
+                                  "destination_city", "destination_country"])
+
+key = ''
+gmaps = googlemaps.Client(key)
+import numpy as np
+import re
+
+def concat_cities(x, y):
+    name = x + "+" + y
+    return(re.sub(" ", "+", name))
+
+locations['origin'] = np.vectorize(concat_cities)(
+    locations['origin_city'], locations['origin_country'])
+locations['destination'] = np.vectorize(concat_cities)(
+    locations['destination_city'], locations['destination_country'])
+print(locations)
+
+
+ham_zur = gmaps.distance_matrix(origins=locations.origin[0], destinations=locations.destination[0],
+                     mode='driving')
+print(ham_zur)
+ham_zur['rows']
+ham_zur['rows'][0]
+ham_zur['rows'][0]['elements']
+map_data = pd.Series(np.vectorize(gmaps.distance_matrix)(locations['origin'],
+                                                      locations['destination'],
+                                                      mode='driving')).map(
+    lambda x: x['rows'][0]['elements'][0])
+map_data
+locations['distance_m'] = [x['distance']['value'] for x in map_data]
+locations['durations_min'] = [x['duration']['value'] for x in map_data]
+locations.drop(['origin', 'destination'], axis=1, inplace=True)
+locations
